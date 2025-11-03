74d9d72aecde0f6c748bf5c284a65b559ca526e1
Akshat <sharmaakshat775@gmail.com>
2025-11-03 12:51:06 +0530

    crash fix

--- android/app/src/main/java/com/dummyproject/MainActivity.kt
+++ android/app/src/main/java/com/dummyproject/MainActivity.kt
@@ -1,27 +1,14 @@
-package com.rndiffapp
+package com.dummyproject
 
-import android.app.Application
-import com.facebook.react.PackageList
-import com.facebook.react.ReactApplication
-import com.facebook.react.ReactHost
-import com.facebook.react.ReactNativeApplicationEntryPoint.loadReactNative
-import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
+import com.facebook.react.ReactActivity
+import com.facebook.react.ReactActivityDelegate
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
+import com.facebook.react.defaults.DefaultReactActivityDelegate
 
-class MainApplication : Application(), ReactApplication {
+class MainActivity : ReactActivity() {
 
-  override val reactHost: ReactHost by lazy {
-    getDefaultReactHost(
-      context = applicationContext,
-      packageList =
-        PackageList(this).packages.apply {
-          // Packages that cannot be autolinked yet can be added manually here, for example:
-          // add(MyReactNativePackage())
-        },
-    )
-  }
+  override fun getMainComponentName(): String = "DummyProject"
 
-  override fun onCreate() {
-    super.onCreate()
-    loadReactNative(this)
-  }
+  override fun createReactActivityDelegate(): ReactActivityDelegate =
+    DefaultReactActivityDelegate(this, getMainComponentName(), fabricEnabled)
 }
